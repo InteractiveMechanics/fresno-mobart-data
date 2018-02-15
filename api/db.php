512f08fce1816244e83751c67baaba6e4a2e334d
Michael Tedeschi <mike@interactivemechanics.com>
2018-02-15 09:57:30 -0700

    Change path for database

--- api/db.php
+++ api/db.php
@@ -3,7 +3,7 @@ function getDB() {
     $dbhost = "localhost";
     $dbuser = "iaccessf_moodle";
     $dbpass = "@48;pbjKz2Q4";
-    $dbname = "iaccessf_stagingmobart";
+    $dbname = "iaccessf_mobart";
     $dbConnection = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbuser, $dbpass); 
     $dbConnection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
