95309f7ff0b293c1c96f450a34fab7a8f16846b5
Michael Tedeschi <mike@interactivemechanics.com>
2018-02-15 09:49:57 -0700

    Adjust #28

--- api/db.php
+++ api/db.php
@@ -3,7 +3,7 @@ function getDB() {
     $dbhost = "localhost";
     $dbuser = "iaccessf_moodle";
     $dbpass = "@48;pbjKz2Q4";
-    $dbname = "iaccessf_mobart";
+    $dbname = "iaccessf_stagingmobart";
     $dbConnection = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbuser, $dbpass); 
     $dbConnection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
